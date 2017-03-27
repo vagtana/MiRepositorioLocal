8f1a06f3d9e1c98386230d30e4c24eb9be79c72c
vero <vero@vero-PC.localdomain>
2017-03-27 23:23:24 +0200

    segundo commit

--- PruebaGIT/src/principal/Principal.java
+++ PruebaGIT/src/principal/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Comentario echo desde segunda ventana eclipse
 		Calculadora c1 = new Calculadora();
 		System.out.println(c1.suma(2,2));
 	}
